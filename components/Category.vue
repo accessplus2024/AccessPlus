0a4a7021d9fb02f31acf8f0121655124e217cdee
castelli <160615799+castlelli@users.noreply.github.com>
2025-02-04 12:02:35 -0300

    feat: :iphone: LP Responsiva
    Correção dos links, paddings, alinhamento dos ícones e botões, tamanho dos ícones e estrutura do componente categoria

--- components/Category.vue
+++ components/Category.vue
@@ -6,11 +6,14 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-gray-100 rounded-lg p-6 flex items-center justify-center">
-    <img :src="`/images/i-${icon}.svg`" />
+  <div
+    class="bg-gray-100 rounded-lg flex items-center justify-start p-3 md:p-6">
+    <img 
+      :src="`/images/i-${icon}.svg`" 
+      class="w-8 h-8 md:w-auto md:h-auto flex-shrink-0 mr-3"
+    />
     <span
-      class="text-black border-l pl-3 ml-3 border-stone-900 h-10 flex justify-center items-center"
-      >{{ category }}</span
-    >
+      class="text-black border-l border-stone-900 pl-3 min-h-[2.5rem] flex items-center break-words text-left text-sm md:text-base"
+    >{{ category }}</span>
   </div>
 </template>
