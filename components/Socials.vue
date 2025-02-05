ff57da46b0548d49b7f8792419b976fc5459626f
accessplus2024 <contact.accessplus@gmail.com>
2025-02-05 11:14:36 -0300

    Update Socials.vue

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,12 +1,12 @@
 <template>
   <div :class="['flex space-x-4 justify-center', themeClass]">
-    <a href="#"
+    <a href="https://www.youtube.com/@AccessPlusOficial"
       ><Youtube class="hover:text-gray-900 transition duration-300"
     /></a>
-    <a href="#"
+    <a href="https://www.instagram.com/accessplusoficial/"
       ><Instagram class="hover:text-gray-900 transition duration-300"
     /></a>
-    <a href="#"
+    <a href="https://www.linkedin.com/company/accesspluseducation"
       ><Linkedin class="hover:text-gray-900 transition duration-300"
     /></a>
   </div>
