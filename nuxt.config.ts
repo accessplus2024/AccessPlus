eab2c0ed5cd3b08e8645c2d1e20b0476007f951e
Camily Carvalho <camily@playersonly.ai>
2025-02-20 13:28:06 -0300

    adicionando analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
+// nuxt.config.ts
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  nitro: { compatibilityDate: "2024-04-03" },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
   modules: ["@nuxt/fonts"],
+  plugins: [{ src: '~/plugins/vercel-analytics.client.ts', mode: 'client' }],
 });
