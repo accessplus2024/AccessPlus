e5c1425d0f36c4dcabbe72b09e0ead752a72cc0b
Camily Carvalho <camily@playersonly.ai>
2025-07-17 12:10:10 -0300

    fix: fix cta margin

--- components/newsletter/Cta.vue
+++ components/newsletter/Cta.vue
@@ -1,14 +1,14 @@
 <template>
   <section
     id="newsletter-subscription"
-    class="bg-gradient-to-r from-purple-600 to-pink-600 pt-12 pb-4 my-10 shadow-xl rounded-lg max-w-7xl mx-auto"
+    class="bg-gradient-to-r from-purple-600 to-pink-600 pt-12 pb-4 my-10 shadow-xl rounded-lg max-w-7xl ml-4 mr-4 sm:ml-6 sm:mr-6 md:mx-auto"
     data-aos="fade-up"
   >
     <div class="text-center mb-8">
       <h3 class="text-2xl md:text-3xl font-bold text-white mb-4">
         Não perca nenhuma novidade!
       </h3>
-      <p class="text-white/90 mb-8 max-w-2xl mx-auto">
+      <p class="text-white/90 mb-8 max-w-2xl mx-auto px-4">
         Inscreva-se na nossa newsletter e receba semanalmente as melhores
         oportunidades e dicas para turbinar sua jornada educacional.
       </p>
