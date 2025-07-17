b6aa214786b8b8d5d357db03575967b48eba1d03
Camily Carvalho <camily@playersonly.ai>
2025-07-17 12:11:28 -0300

    fix: remove unwanted right margin whitespace in opportunities

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
+  <div class="relative overflow-x-hidden">
   <div class="absolute top-0 left-0 w-full z-30">
     <Navbar transparent />
   </div>
   <slot></slot>
   <Footer />
+  </div>
 </template>
