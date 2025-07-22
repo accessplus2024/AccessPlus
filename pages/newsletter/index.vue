f2bd9626e2a0055d687aefce0969e034ee65b5b0
accessplus2024 <contact.accessplus@gmail.com>
2025-07-22 19:08:40 -0300

    Fix duplicate access+ name in page title

--- pages/newsletter/index.vue
+++ pages/newsletter/index.vue
@@ -5,7 +5,7 @@ import "aos/dist/aos.css";
 
 // SEO and Meta
 useHead({
-  title: "Newsletter - Access+",
+  title: "Newsletter",
   meta: [
     { charset: "UTF-8" },
     { name: "viewport", content: "width=device-width, initial-scale=1.0" },
