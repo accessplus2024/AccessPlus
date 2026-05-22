1392cfee11cf909d64c0bd8e6ff1c8c748903ca5
João Pedro Marques Santos <santosjoaopedroms@gmail.com>
2026-05-22 14:12:08 -0300

    fix: use SunLight icon (Sun is not exported by @iconoir/vue)

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -4,13 +4,13 @@
     class="p-2 rounded-full hover:bg-white/10 transition-colors"
     :title="modeLabel"
   >
-    <Sun v-if="colorMode.value !== 'dark'" class="w-5 h-5" />
+    <SunLight v-if="colorMode.value !== 'dark'" class="w-5 h-5" />
     <HalfMoon v-else class="w-5 h-5" />
   </button>
 </template>
 
 <script setup>
-import { Sun, HalfMoon } from '@iconoir/vue'
+import { SunLight, HalfMoon } from '@iconoir/vue'
 
 const colorMode = useColorMode()
 const modes = ['system', 'light', 'dark']
